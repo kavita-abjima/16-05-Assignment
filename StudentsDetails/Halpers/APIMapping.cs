cf7387428b6382200225d04d2a057538f1d7d5bc
Kavita <kavita.jangir@abjima.com>
2023-05-16 17:21:17 +0530

    StudentAPI

---
+++ StudentsDetails/Halpers/APIMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using StudentsDetails.Dto;
+using StudentsDetails.Models;
+
+namespace StudentsDetails.Halpers
+{
+    public class APIMapping:Profile
+    {
+        public APIMapping()
+        {
+            CreateMap<StudentsDetailDto, Student>();
+        }
+    }
+}
